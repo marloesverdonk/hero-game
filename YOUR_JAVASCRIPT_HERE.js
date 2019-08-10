2c21e7498c1702ab795d0319c3e139bcd965c2dc
marloesverdonk <marloesverdonk@live.nl>
2019-08-10 12:24:34 +0200

    Object and resting

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,33 @@
-// Write your JS here
+const hero = {
+    name: "Aart",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "Knife",
+        damage: 2,
+    }
+}
+
+function rest(hero) {
+    if (hero.health == 10) {
+        alert("Health already has value of 10")
+    }
+    else {
+        hero.health = 10
+    }
+    return hero
+}
+
+const img = document.getElementById('inn')
+img.addEventListener("click", function () {rest(hero)})
+
+
+
+function pickUpItem() {
+
+}
+
+function equipWeapon() {
+
+}
