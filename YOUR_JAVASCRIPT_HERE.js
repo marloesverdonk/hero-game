0623865ec9acfec40ac1f32d602c3a6e760c51ac
marloesverdonk <marloesverdonk@live.nl>
2019-08-10 19:04:28 +0200

    Homework done

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,10 +1,10 @@
 const hero = {
-    name: "Aart",
+    name: "Marloes",
     heroic: true,
     inventory: [],
     health: 10,
     weapon: {
-        type: "Knife",
+        type: "gun",
         damage: 2,
     }
 }
@@ -25,6 +25,7 @@ inn.addEventListener("click", function () { rest(hero) })
 
 function pickUpItem(hero, weapon) {
     hero.inventory.push(weapon)
+    displayStats(hero)
 }
 
 const dagger = document.getElementById('dagger')
@@ -39,8 +40,35 @@ function equipWeapon(hero) {
     if (hero.inventory.length > 0) {
         hero.weapon = hero.inventory[0]
     }
+    displayStats(hero)
 }
 
 const bag = document.getElementById('bag')
-bag.addEventListener("click", function(){ equipWeapon(hero) })
+bag.addEventListener("click", function () { equipWeapon(hero) })
+
+const gun = document.getElementById('gun')
+const weapon = hero.weapon
+gun.addEventListener("click", function () { pickUpItem(hero, weapon) })
+
+function changeHeroName(hero) {
+    const nameField = document.getElementById('name');
+    const newName = nameField.value
+    hero.name = newName
+    displayStats(hero)
+}
+
+function displayStats(hero) {
+    const statsdisplay = document.getElementById('display')
+    statsdisplay.innerText = JSON.stringify(hero, null, 2)
+    return hero
+}
+
+console.log(hero)
+displayStats(hero)
+
+// - Write `displayStats` function that writes your hero's name, health, weapontype, weapon damage to the page. Call it at the end of your script
+
+// - Create a form that allow users to change the name of their hero.
+
+// - Call `displayStats` when some data of the hero has changed to update the page.
 
